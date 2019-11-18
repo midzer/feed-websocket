31f2621bd7b06ffb3d1c6cad5354fca8aab2ec0a
midzer <midzer@gmail.com>
2019-11-18 19:56:43 +0100

    initial version

---
+++ README.md
@@ -0,0 +1,8 @@
+# feed-websocket
+
+Node.js WebSocket server who delivers feeds gathered via JSON API
+
+## Usage
+
+* Set `API_ENDPOINT` for your (external) JSON file containing an Array of RSS feed URLs
+* Set `WEBSOCKET_PORT` for a port this server should listen
---
+++ index.js
@@ -0,0 +1,140 @@
+var app = require('express')();
+var http = require('http');
+var server = http.createServer(app);
+
+var WebSocket = require('ws');
+var wss = new WebSocket.Server({ server });
+
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const RssFeedEmitter = require('rss-feed-emitter');
+const feeder = new RssFeedEmitter();
+
+const request = require('request');
+
+const API_ENDPOINT = 'http://localhost:4000/feeds.json';
+const WEBSOCKET_PORT = 63409;
+
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+
+// Globals
+let connectMessage;
+let newItem;
+let updatingFeeds;
+
+function updateFeeds () {
+  // Stop updateCache() recursion
+  updatingFeeds = true;
+
+  // Clear db
+  db.unset('log')
+    .write()
+  
+  // Set some defaults
+  db.defaults({ log: [] })
+    .write();
+
+  // Remove all feeds
+  feeder.destroy();
+
+  request({
+    url: API_ENDPOINT,
+    json: true
+    },
+    function (error, response, body) {
+      body.forEach(feed => {
+        // Add all feeds
+        feeder.add({
+          url: feed
+        });
+      });
+    }
+  );
+  // Allow updateCache() recursion again
+  updatingFeeds = false;
+
+  // Give feeder some time to fetch all feeds
+  setTimeout(() => {
+    updateCache();
+  }, 30000);
+
+  // Update feeds daily
+  setTimeout(() => {
+    updateFeeds();
+  }, 24 * 60 * 60 * 1000);
+}
+
+function updateCache () {
+  if (newItem) {
+    const entries = db.get('log')
+                      .sortBy('date')
+                      .takeRight(25)
+                      .value();
+    connectMessage = JSON.stringify(entries);
+
+    // Wait for another new item first
+    newItem = false;
+  }
+  // Start recursion
+  setTimeout(() => {
+    if (!updatingFeeds) {
+      updateCache();
+    }
+  }, 1000);
+}
+
+feeder.on('new-item', item => {
+  // Skip already existing links
+  const link = db.get('log')
+                 .find({ link: item.link })
+                 .value()
+  if (link) {
+    return;
+  }
+  // Push to db
+  db.get('log')
+    .push({ title: item.title, date: item.date, link: item.link})
+    .write()
+  
+  // Send to all connected clients immediately
+  wss.clients.forEach(function(client) {
+    if (client.readyState === WebSocket.OPEN ) {
+      client.send(JSON.stringify([{
+        title: item.title,
+        date: item.date,
+        link: item.link
+      }]));
+    }
+  });
+  // Allow cache to be updated
+  newItem = true;
+});
+
+wss.on('connection', function(socket) {
+  console.log('a user connected');
+  if (socket.readyState === WebSocket.OPEN) {
+    if (connectMessage) {
+      socket.send(connectMessage);
+    }
+    else {
+      // On launch connectMessage might still be empty
+      socket.send(JSON.stringify([{
+        title: "Server ist gerade beschäftigt, bitte Seite neu laden",
+        date: new Date().toISOString(),
+        link: "/"
+      }]));
+    }
+  }
+
+  socket.on('close', function() {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(WEBSOCKET_PORT, function() {
+  console.log(`listening on *:${WEBSOCKET_PORT}`);
+});
+// Kickstart
+updateFeeds();
